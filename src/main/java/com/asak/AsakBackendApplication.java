c0256d966fee11e814ef3bfb567adc55965d3b19
kimnayeon <nayeun9198@gmail.com>
2026-07-22 16:42:32 +0900

    패키지 수정

--- src/main/java/com/asak/AsakBackendApplication.java
+++ src/main/java/com/asak/AsakBackendApplication.java
@@ -1,4 +1,4 @@
-package com.asak.backend;
+package com.asak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/asak/AsakBackendApplicationTests.java
+++ src/test/java/com/asak/AsakBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.asak.backend;
+package com.asak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
