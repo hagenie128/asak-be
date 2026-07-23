9c413e9e9d250cdf2e296e19a13c6d8db53b2df1
hagenie128 <wlslzz128@gmail.com>
2026-07-23 16:34:36 +0900

    update: common ApiResponse 성공 실패 응답 추가

--- src/main/java/com/asak/common/response/ApiResponse.java
+++ src/main/java/com/asak/common/response/ApiResponse.java
@@ -25,4 +25,24 @@ public class ApiResponse<T> {
   private String code;
   private String message;
   private T data;
+
+  public static <T> ApiResponse<T> success(T data) {
+    return ApiResponse.<T>builder()
+        .success(true)
+        .status(200)
+        .code("SUCCESS")
+        .message("요청이 성공했습니다.")
+        .data(data)
+        .build();
+  }
+
+public static <T> ApiResponse<T> failure(int status, String code, String message) {
+    return ApiResponse.<T>builder()
+        .success(false)
+        .status(status)
+        .code(code)
+        .message(message)
+        .data(null)
+        .build();
 }
+
