76fda07ef27fb14c179e1f84d9afe5b5224f25cd
kimnayeon <nayeun9198@gmail.com>
2026-07-23 15:15:52 +0900

    update: common ApiResponse 응답 추가

--- src/main/java/com/asak/common/response/ApiResponse.java
+++ src/main/java/com/asak/common/response/ApiResponse.java
@@ -1,6 +1,30 @@
 package com.asak.common.response;
 
-//모든 API의 반환 규격
+import lombok.Builder;
+import lombok.Getter;
 
-public class ApiResponse {
+// -- [응답 공통 Api] --
+// {
+//   "success": true,
+//   "status": 200,
+//   "code": "SUCCESS",
+//   "message": "OK",
+//   "data": {...
+//   }
+// }
+
+//모든 API의 반환 규격
+// ** T는 제네릭(Generic)
+
+@Getter
+@Builder
+public class ApiResponse<T> {
+
+    private boolean success;
+    private int status;
+    private String code;
+    private String message;
+    private T data;
+
+
 }
--- src/main/java/com/asak/user/dto/CreateOrderResponse.java
+++ src/main/java/com/asak/user/dto/CreateOrderResponse.java
@@ -1,11 +1,24 @@
 package com.asak.user.dto;
 
+import com.asak.common.enums.OrderStatus;
 
+import lombok.Builder;
+import lombok.Getter;
 
+//   "data": {
+//     "orderId": 1,
+//     "orderNo": "A202607230001",
+//     "totalAmount": 8900,
+//     "status": "RECEIVED"
+//   }
 
+@Getter
+@Builder
 public class CreateOrderResponse {
 
-
-
+    private Long orderId;
+    private String orderNo;
+    private Integer totalAmount;
+    private OrderStatus status;
 
 }
