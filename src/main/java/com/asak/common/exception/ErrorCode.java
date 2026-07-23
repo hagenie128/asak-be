0ce5fb659c31ebc24b2b4181a32a6f602fc0b6f6
hagenie128 <wlslzz128@gmail.com>
2026-07-23 16:41:36 +0900

    update: common ErrorCode 작성

--- src/main/java/com/asak/common/exception/ErrorCode.java
+++ src/main/java/com/asak/common/exception/ErrorCode.java
@@ -1,5 +1,25 @@
 package com.asak.common.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
 // Service 로직에서 터지는 에러를 공통 처리
 
-public class ErrorCode {}
+@Getter
+public enum ErrorCode {
+  MENU_NOT_FOUND(HttpStatus.NOT_FOUND),
+  MENU_SOLD_OUT(HttpStatus.CONFLICT),
+  INVALID_OPTION_SELECTION(HttpStatus.BAD_REQUEST),
+  ORDER_PRICE_CHANGED(HttpStatus.CONFLICT),
+  ORDER_NOT_FOUND(HttpStatus.NOT_FOUND),
+  INVALID_ORDER_STATUS_TRANSITION(HttpStatus.CONFLICT),
+  PAYMENT_METHOD_DISABLED(HttpStatus.CONFLICT),
+  PAYMENT_ALREADY_APPROVED(HttpStatus.CONFLICT),
+  PAYMENT_FAILED(HttpStatus.BAD_REQUEST);
+
+  private final HttpStatus status;
+
+  ErrorCode(HttpStatus status) {
+    this.status = status;
+  }
+}
