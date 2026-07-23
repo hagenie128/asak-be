645776552b35ec2d5af3dd17e66c7c0f0349645b
kimnayeon <nayeun9198@gmail.com>
2026-07-23 15:03:53 +0900

    update : api-005 작업 백업

---
+++ src/main/java/com/asak/user/dto/OptionItemRequest.java
@@ -0,0 +1,11 @@
+package com.asak.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class OptionItemRequest {
+
+    private Long optionItemId;
+    private Integer quantity;
+
+}
--- src/main/java/com/asak/user/dto/request.java
+++
@@ -1,6 +0,0 @@
-package com.asak.user.dto;
-
-//@Alias("")//xml에서 별칭으로 호출가능
-
-public class request {
-}
