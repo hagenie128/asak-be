7d891f74e16119eb08e997eb0d0a5d8fa130150b
kimnayeon <nayeun9198@gmail.com>
2026-07-24 14:57:51 +0900

    update : api-005 주문 생성 과정 백업

--- src/main/java/com/asak/user/controller/UserOrderController.java
+++ src/main/java/com/asak/user/controller/UserOrderController.java
@@ -3,9 +3,15 @@ package com.asak.user.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.asak.common.response.ApiResponse;
+import com.asak.user.dto.CreateOrderRequest;
+import com.asak.user.dto.CreateOrderResponse;
 import com.asak.user.service.UserOrderService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 // ---[주문]---
 // 장바구니 검증
@@ -18,6 +24,15 @@ public class UserOrderController {
 
     //주문 서비스 연결
     private final UserOrderService orderService;
+
+    @PostMapping("/orders")
+    public ApiResponse<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest request){
+
+        CreateOrderResponse response = orderService.createOrder(request);
+        return ApiResponse.success(response);
+    }
+
+    
 
 
 
