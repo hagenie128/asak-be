3f98c69dff071612848eb2186d6f39830c09f772
kimnayeon <nayeun9198@gmail.com>
2026-08-13 15:07:45 +0900

    update: enums에 토스페이 추가

--- src/main/java/com/asak/common/enums/PaymentMethod.java
+++ src/main/java/com/asak/common/enums/PaymentMethod.java
@@ -1,8 +1,9 @@
 package com.asak.common.enums;
 
 public enum PaymentMethod {
-  // 카드 단말 결제(삼성페이 포함), 카카오페이, 네이버페이
+  // 카드 단말 결제(삼성페이 포함), 카카오페이, 네이버페이, 토스페이
   CARD,
   KAKAO_PAY,
-  NAVER_PAY
+  NAVER_PAY,
+  TOSS_PAY
 }
