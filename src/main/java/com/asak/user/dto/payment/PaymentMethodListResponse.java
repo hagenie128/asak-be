ecb15c8872ceb35acb13a4aff989c49927392517
kimnayeon <nayeun9198@gmail.com>
2026-08-18 00:04:58 +0900

    update : db컬럼명 변경 반영 & api-014 명세표 추가수정

--- src/main/java/com/asak/user/dto/payment/PaymentMethodListResponse.java
+++ src/main/java/com/asak/user/dto/payment/PaymentMethodListResponse.java
@@ -8,30 +8,50 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+// "data": {
 //     "methods": [
-    //       {
-    //         "methodId": 10828,
-    //         "methodCode": "CARD",
-    //         "methodName": "카드·삼성페이",
-    //         "isEnabled": true,
-    //         "sortOrder": 1
-    //       },
-    //       {
-    //         "methodId": 10829,
-    //         "methodCode": "KAKAO_PAY",
-    //         "methodName": "카카오페이",
-    //         "isEnabled": false,
-    //         "sortOrder": 2
-    //       },
-    //       {
-    //         "methodId": 10830,
-    //         "methodCode": "NAVER_PAY",
-    //         "methodName": "네이버페이",
-    //         "isEnabled": false,
-    //         "sortOrder": 3
-    //       }
-    //     ]
-
+//       {
+//         "methodId": 10828,
+//         "methodCode": "CARD",
+//         "methodName": "카드 / 삼성페이 결제",
+//         "imageAssetId": 352,
+//         "imageUrl": "/api/assets/101",
+//         "description": "신용·체크카드",
+//         "isEnabled": true,
+//         "sortOrder": 1
+//       },
+//       {
+//         "methodId": 10829,
+//         "methodCode": "KAKAO_PAY",
+//         "methodName": "카카오페이 결제",
+//         "imageAssetId": 353,
+//         "imageUrl": "/api/assets/101",
+//         "description": "모바일 간편결제",
+//         "isEnabled": true,
+//         "sortOrder": 2
+//       },
+//       {
+//         "methodId": 10830,
+//         "methodCode": "NAVER_PAY",
+//         "methodName": "네이버페이 결제",
+//         "imageAssetId": 354,
+//         "imageUrl": "/api/assets/101",
+//         "description": "모바일 간편결제",
+//         "isEnabled": true,
+//         "sortOrder": 3
+//       },
+//       {
+//         "methodId": 10832,
+//         "methodCode": "TOSS_PAY",
+//         "methodName": "토스페이 결제",
+//         "imageAssetId": 355,
+//         "imageUrl": "/api/assets/101",
+//         "description": "모바일 간편결제",
+//         "isEnabled": true,
+//         "sortOrder": 4
+//       }
+//     ]
+//   }
 
 // 결제수단 methods들의 list[]
 @Getter
